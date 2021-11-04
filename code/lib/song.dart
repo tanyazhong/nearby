34cd2292c64c53371efef46b3fcd8c33283164b8
rachel oberst <rae.oberst@gmail.com>
2021-11-04 16:15:34 -0700

    continuation

--- code/lib/song.dart
+++ code/lib/song.dart
@@ -1,9 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:spotify/spotify.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:uni_links/uni_links.dart';
 
+
+//https://pub.dev/documentation/uni_links/latest/ for uni_links
 const String clientID = "b03425c1e1af4ba1bc82f71a5bc0875b";
 const String clientSecret = "2bcdc8148d674bfa92507e10280971f4";
+const String redirectURI = "com.example.nearby://callback/success";
+final scopes = ["user-read-email", "user-library-read"];
 
 class Song extends StatefulWidget{
   @override
@@ -12,15 +21,84 @@ class Song extends StatefulWidget{
 
 class _SongState extends State<Song> {
 
+  SpotifyApiCredentials? credentials;
   var spotify;
+  String? responseURI = null;
+  StreamSubscription? subscription;
+  bool checkedInitialUri = false;
+  String? currentLink;
 
   @override
   initState() {
-    SpotifyApiCredentials credentials = SpotifyApiCredentials(
+    super.initState();
+    credentials = SpotifyApiCredentials(
         clientID, clientSecret);
-    spotify = SpotifyApi(credentials);
-    print("passed");
+    spotify = SpotifyApi(credentials!);
+    print("passed spotify credentials, no user yet");
+    initialLink();
+    print('passed initial uri link');
   }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    subscription!.cancel();
+  }
+  //checks the initial uri link, called on initState
+  void initialLink() async {
+    if(!checkedInitialUri){
+      checkedInitialUri = true;
+      try {
+        final link = await getInitialLink();
+        if(link == null){
+          print("no initial link");
+        }
+        else{
+          print("initial link is $link");
+        }
+        if(!mounted) return;
+        setState(() {
+          currentLink = link;
+        });
+      }on PlatformException{
+        print("failed to get initial link");
+      }on FormatException catch (err){
+        if (!mounted) return;
+        print('malformed initial link');
+      }
+    }
+  }
+
+  void connectToUser() async{
+    final grant = SpotifyApi.authorizationCodeGrant(credentials!);
+    final authUri = grant.getAuthorizationUrl(Uri.parse(redirectURI), scopes: scopes,);
+    print("authrui is ${authUri.toString()}");
+    //opens browser to take user to spotify authorization page
+
+    final result = await canLaunch((authUri.toString()));
+    print('result is $result');
+    if (await canLaunch(authUri.toString())){
+      print("can launch");
+      launch(authUri.toString());
+
+    }
+    print("passed canlaunch");
+    //listen for redirection back to app from authorization page
+    subscription = linkStream.listen((String? link){
+      print("current link is $currentLink");
+      if (link!.startsWith(redirectURI)){
+        setState(() {
+          currentLink = link;
+        });
+         print("success");
+      }
+      }, onError: (err){
+        print("error listening to redirect");
+     });
+    print("passed listen");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +118,11 @@ class _SongState extends State<Song> {
                   var currently = await spotify.me.currentlyPlaying();
                   print(currently);
                 },
-                child: Text("press for api call"),)
+                  child: Text("press for api call"),),
+                TextButton(onPressed: () async {
+                  connectToUser();
+                },
+                  child: Text("user log in"),),
               ]
 
     ),
