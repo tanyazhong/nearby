6166800eb649c904a091788116882e327e7a89b0
Tanya Zhong <tanyazhong1@gmail.com>
2021-11-11 10:07:09 -0800

    FutureBuilder

--- code/lib/pages/grid_view_page.dart
+++ code/lib/pages/grid_view_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:my_app/logic/database.dart';
 import 'package:spotify/spotify.dart';
@@ -52,20 +53,101 @@ class _GridViewPageState extends State<GridViewPage> {
     print('radius is $_radius, grid view is $_gridView');
   }
 
+//   @override
+//   Widget build(BuildContext context) {
+//     final argumentSpotify =
+//         ModalRoute.of(context)!.settings.arguments as SpotifyApi;
+//     FilterValues filterValues = FilterValues();
+//     Future<List<dynamic>> futureNearbySongIds =
+//         MongoDatabase.getNearbySongsForLoc(100, 100, 10);
+//     print("song ids $futureNearbySongIds");
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Grid View"),
+//         actions: [
+//           Padding(
+//               padding: EdgeInsets.only(right: 20, top: 18),
+//               child: GestureDetector(
+//                 onTap: () {
+//                     Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (_) => FilterPage(
+//                                   filterValues: filterValues,
+//                                   onChanged: _onRadiusChanged,
+//                                 )));
+//                   },
+//                   child: Text(
+//                     'Filter',
+//                     style: TextStyle(fontSize: 15, color: Colors.white),
+//                   )))
+//         ],
+//       ),
+//       body: Center(
+//           child: GridView.count(
+//         primary: false,
+//         padding: const EdgeInsets.all(15),
+//         crossAxisSpacing: 10,
+//         mainAxisSpacing: 10,
+//         crossAxisCount: 3,
+//         children: <Widget>[
+//           Container(
+//             padding: const EdgeInsets.all(8),
+//             alignment: Alignment.topLeft,
+//             child: TextButton(
+//                 onPressed: () {
+//                   goToSongPage(argumentSpotify);
+//                 },
+//                 child: const Text('Heed not the rabble',
+//                     style: TextStyle(color: Colors.black))),
+//             color: Colors.teal[200],
+//           ),
+
+//           Container(
+//             padding: const EdgeInsets.all(2),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: <Widget>[
+//                 Container(
+//                   child: widgets.Image.asset("assets/nearby_logo.png",
+//                       alignment: Alignment.center, height: 80, width: 80),
+//                   color: Colors.white,
+//                 ),
+//                 const Text(
+//                   'Song Title',
+//                   textAlign: TextAlign.left,
+//                 ),
+//                 const Text(
+//                   'Song Artist',
+//                   textAlign: TextAlign.left,
+//                 ),
+//               ],
+//             ),
+//             color: Colors.white,
+//           ),
+//         ],
+//       )),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _updateText,
+//         tooltip: 'Update Text',
+//         child: Icon(Icons.library_music),
+//       ),
+//     );
+//   }
+// }
+
+// https://api.flutter.dev/flutter/widgets/FutureBuilder-class.html
   @override
   Widget build(BuildContext context) {
     final argumentSpotify =
         ModalRoute.of(context)!.settings.arguments as SpotifyApi;
     FilterValues filterValues = FilterValues();
-    Future<List<dynamic>> nearbySongIds =
-        MongoDatabase.getNearbySongsForLoc(100, 100, 10);
-    print("song ids $nearbySongIds");
     return Scaffold(
       appBar: AppBar(
-        title: Text("Grid View"),
+        title: const Text("Grid View"),
         actions: [
           Padding(
-              padding: EdgeInsets.only(right: 20, top: 18),
+              padding: const EdgeInsets.only(right: 20, top: 18),
               child: GestureDetector(
                   onTap: () {
                     Navigator.push(
@@ -76,89 +158,80 @@ class _GridViewPageState extends State<GridViewPage> {
                                   onChanged: _onRadiusChanged,
                                 )));
                   },
-                  child: Text(
+                  child: const Text(
                     'Filter',
                     style: TextStyle(fontSize: 15, color: Colors.white),
                   )))
         ],
       ),
-      body: Center(
-          child: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(15),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 3,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.topLeft,
-            child: TextButton(
-                onPressed: () {
-                  goToSongPage(argumentSpotify);
-                },
-                child: const Text('Heed not the rabble',
-                    style: TextStyle(color: Colors.black))),
-            color: Colors.teal[200],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Sound of screams but the'),
-            color: Colors.teal[300],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Who scream'),
-            color: Colors.teal[400],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Revolution is coming...'),
-            color: Colors.teal[500],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Revolution, they...'),
-            color: Colors.teal[600],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text("He'd have you all unravel at the"),
-            color: Colors.teal[100],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Heed not the rabble'),
-            color: Colors.teal[200],
-          ),
-          Container(
-            padding: const EdgeInsets.all(2),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  child: widgets.Image.asset("assets/nearby_logo.png",
-                      alignment: Alignment.center, height: 80, width: 80),
-                  color: Colors.white,
-                ),
-                const Text(
-                  'Song Title',
-                  textAlign: TextAlign.left,
-                ),
-                const Text(
-                  'Song Artist',
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            color: Colors.white,
-          ),
-        ],
-      )),
+      body: FutureBuilder<dynamic>(
+        future: MongoDatabase.getNearbySongsForLoc(100, 100, 10),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[];
+            print("data: ${snapshot.data}");
+            return Center(
+              child: 
+              Text(
+                'Result: ${snapshot.data}',
+              ),
+              // GridView.builder(
+              //     gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              //         maxCrossAxisExtent: 200,
+              //         childAspectRatio: 3 / 2,
+              //         crossAxisSpacing: 20,
+              //         mainAxisSpacing: 20),
+              //     itemCount: snapshot.data.length,
+              //     itemBuilder: (BuildContext ctx, index) {
+              //       return Container(
+              //         alignment: Alignment.center,
+              //         child: const Text("hello"),
+              //       );
+              //     }),
+            );
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Text('Error: ${snapshot.error}'),
+            ];
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: children,
+              ),
+            );
+          } else {
+            children = const <Widget>[
+              SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Awaiting result...'),
+              )
+            ];
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: children,
+              ),
+            );
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _updateText,
         tooltip: 'Update Text',
-        child: Icon(Icons.library_music),
+        child: const Icon(Icons.library_music),
       ),
     );
   }
