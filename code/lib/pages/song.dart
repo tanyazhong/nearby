f451806861cb829fe2b848dac1bed18c89fa5f9a
rachel oberst <rae.oberst@gmail.com>
2021-11-18 15:47:00 -0800

    created super class for song page and song widget to extend

--- code/lib/pages/song.dart
+++ code/lib/pages/song.dart
@@ -9,36 +9,129 @@ import 'package:transparent_image/transparent_image.dart';
 
 
 
+abstract class SongState extends State<StatefulWidget> {
+  ///Song ID that can be used with the spotify API to get the song's track, set in constructor
+  String trackID;
+  double fontSize;
+
+  SongState({ required this.trackID, required this.fontSize});
+
+  Widget build(BuildContext context);
+  void onTap();
+
+  ///Displays the list of artists
+  Widget artistList(Track track) {
+    if (track.name == null) {
+      return Text('Artist not available');
+    }
+    List<Widget> list = [];
+    String? temp;
+    int length = track.artists!.length;
+    if (length == 0) {
+      return Text('Artist not available');
+    }
+    for (int i = 0; i < length; i++) {
+      if (i == length - 1) {
+        temp = track.artists![i].name;
+      }
+      else {
+        temp = track.artists![i].name! + ', ';
+      }
+      list.add(
+          Text(
+            temp!, style: TextStyle(fontFamily: 'Acme', fontSize: fontSize),));
+    }
+    return Row(children: list, mainAxisAlignment: MainAxisAlignment.center,);
+  }
+
+  Widget songContainer() {
+    return FutureBuilder(
+        future: API()
+            .authenticate()
+            .tracks
+            .get(trackID),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            Track track = snapshot.data;
+            print('track is $track from builder');
+            return GestureDetector(
+              onTap: onTap,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.all(20),
+                            child:
+                            ClipRRect(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(30)),
+                              child:
+                              FadeInImage.memoryNetwork(
+                                  placeholder: kTransparentImage,
+                                  image: API().imageUrl(track.album!)),
+                            )),
+                        Text('${track.name}',
+                          style: TextStyle(fontFamily: 'Acme', fontSize: fontSize+10,),
+                          textAlign: TextAlign.center,),
+                        artistList(track),
+                      ]
+
+                  )
+              ),
+            );
+          }
+          else {
+            return Center(
+              child: Text('Not available',
+                style: TextStyle(fontFamily: 'Acme', fontSize: 20,),
+                textAlign: TextAlign.center,),
+              // ]
+
+              // ),
+            );
+          }
+        }
+    );
+  }
+}
+
 ///Represents the page that will display a song
-class Song extends StatefulWidget{
+class SongPage extends StatefulWidget{
 
   ///Track of the song that will be displayed, holds image url, artist names and song name, set in constructor
-  final Track track;
+  final String trackID;
 
-  const Song({Key? key, required this.track}): super(key: key);
+  const SongPage({Key? key, required this.trackID}): super(key: key);
 
   @override
   ///Creates mutable state for Song at a given location in the tree
-  _SongState createState() => _SongState( track: track);
+  _SongPageState createState() => _SongPageState(trackID: trackID, fontSize: 20);
 }
 
 ///Class that holds the state of [Song] and displays the song page
-class _SongState extends State<Song> {
+class _SongPageState extends SongState {
 
   ///Track of the song that will be displayed, holds image url, artist names and song name, set in constructor
-  Track track;
-  _SongState({ required this.track});
-  String? imageUrl;
 
+  String? imageUrl;
+  String trackID;
+  double fontSize;
+  _SongPageState({required this.trackID, required this.fontSize }) : super(trackID: trackID, fontSize: fontSize);
+
+/*
   ///Retrieves the url of the image of the designated track from the [API class]
   @override
   initState(){
     AlbumSimple album = track.album!;
     imageUrl = API().imageUrl(album);
     print('image url is $imageUrl');
-  }
+  }*/
 
-  ///Displays the list of artists
+
+  void onTap() {}
+  /*///Displays the list of artists
   Widget artistList() {
     List<Widget> list =  [];
     String? temp;
@@ -57,7 +150,7 @@ class _SongState extends State<Song> {
     }
     return Row(children: list, mainAxisAlignment: MainAxisAlignment.center,);
    // return Row(children: track.artists!.map((artist) =>  Text('${artist.name}', style: GoogleFonts.acme(fontSize: 20),)).toList());
-  }
+  }*/
 
   ///Displays the page and calls [artistList()]
   @override
@@ -73,7 +166,8 @@ class _SongState extends State<Song> {
         centerTitle: true,
         toolbarHeight: 50,
       ),
-      body: Center(
+      body: songContainer(),
+      /*Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -90,7 +184,7 @@ class _SongState extends State<Song> {
             ]
 
         ),
-      ),
+      ),*/
     );
   }
 }
@@ -106,19 +200,28 @@ class SongWidget extends StatefulWidget{
 
   @override
   ///Creates mutable state for Song at a given location in the tree
-  _SongWidgetState createState() => _SongWidgetState( trackID: trackID);
+  _SongWidgetState createState() => _SongWidgetState( trackID: trackID, fontSize: 10);
 }
 
 ///Class that holds the state of [SongWidget] and displays the song page
-class _SongWidgetState extends State<SongWidget> {
+class _SongWidgetState extends SongState {
 
   ///Song ID that can be used with the spotify API to get the song's track, set in constructor
   String trackID;
+  double fontSize;
 
-  _SongWidgetState({ required this.trackID});
+  _SongWidgetState({ required this.trackID, required this.fontSize}) : super(trackID: trackID, fontSize: fontSize);
 
-
-  ///Displays the list of artists
+  void onTap() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => SongPage(
+            trackID: trackID,
+          ),
+        ));
+  }
+ /* ///Displays the list of artists
   Widget artistList(Track track) {
     if(track.name == null){
       return Text('Artist not available');
@@ -140,12 +243,13 @@ class _SongWidgetState extends State<SongWidget> {
           Text(temp!, style: TextStyle(fontFamily: 'Acme', fontSize: 10),));
     }
     return Row(children: list, mainAxisAlignment: MainAxisAlignment.center,);
-  }
+  }*/
 
   ///Displays the page and calls [artistList()]
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
+    return songContainer();}
+   /* return FutureBuilder(
         future: API()
             .authenticate()
             .tracks
@@ -155,14 +259,8 @@ class _SongWidgetState extends State<SongWidget> {
             Track track = snapshot.data;
             print('track is $track from builder');
             return GestureDetector(
-              onTap: () {Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => Song(
-                    track: track,
-                    ),
-                ));
-                },
+              onTap: onTap(),
+
               child: Container(
               alignment: Alignment.center,
               child: Column(
@@ -195,12 +293,11 @@ class _SongWidgetState extends State<SongWidget> {
                  // ]
 
              // ),
-            );
+            );*/
           }
-        }
-    );
-  }
-}
+
+
+
 
 /*
 void getCurrentTrack()  {
