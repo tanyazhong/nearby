0d43fc4992e998747bbeba8a98f53bda6faf1b6b
rachel oberst <rae.oberst@gmail.com>
2021-11-19 11:07:07 -0800

    clean up

--- code/lib/pages/song.dart
+++ code/lib/pages/song.dart
@@ -120,37 +120,8 @@ class _SongPageState extends SongState {
   double fontSize;
   _SongPageState({required this.trackID, required this.fontSize }) : super(trackID: trackID, fontSize: fontSize);
 
-/*
-  ///Retrieves the url of the image of the designated track from the [API class]
-  @override
-  initState(){
-    AlbumSimple album = track.album!;
-    imageUrl = API().imageUrl(album);
-    print('image url is $imageUrl');
-  }*/
-
 
   void onTap() {}
-  /*///Displays the list of artists
-  Widget artistList() {
-    List<Widget> list =  [];
-    String? temp;
-    int length = track.artists!.length;
-    if (length == 0){
-      return Text('Artist not available');
-    }
-    for (int i = 0; i< length; i++){
-      if (i == length -1){
-        temp = track.artists![i].name;
-      }
-      else{
-        temp = track.artists![i].name! + ', ';
-      }
-      list.add(Text(temp!, style: TextStyle(fontFamily: 'Acme', fontSize: 20),));
-    }
-    return Row(children: list, mainAxisAlignment: MainAxisAlignment.center,);
-   // return Row(children: track.artists!.map((artist) =>  Text('${artist.name}', style: GoogleFonts.acme(fontSize: 20),)).toList());
-  }*/
 
   ///Displays the page and calls [artistList()]
   @override
@@ -167,24 +138,7 @@ class _SongPageState extends SongState {
         toolbarHeight: 50,
       ),
       body: songContainer(),
-      /*Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child:
-                  ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    child:
-                      FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: imageUrl!),
-              )),
-              Text('${track.name}', style: TextStyle(fontFamily: 'Acme', fontSize: 30,), textAlign: TextAlign.center,),
-              artistList(),
-            ]
 
-        ),
-      ),*/
     );
   }
 }
@@ -221,79 +175,12 @@ class _SongWidgetState extends SongState {
           ),
         ));
   }
- /* ///Displays the list of artists
-  Widget artistList(Track track) {
-    if(track.name == null){
-      return Text('Artist not available');
-    }
-    List<Widget> list = [];
-    String? temp;
-    int length = track.artists!.length;
-    if (length == 0) {
-      return Text('Artist not available');
-    }
-    for (int i = 0; i < length; i++) {
-      if (i == length - 1) {
-        temp = track.artists![i].name;
-      }
-      else {
-        temp = track.artists![i].name! + ', ';
-      }
-      list.add(
-          Text(temp!, style: TextStyle(fontFamily: 'Acme', fontSize: 10),));
-    }
-    return Row(children: list, mainAxisAlignment: MainAxisAlignment.center,);
-  }*/
+
 
   ///Displays the page and calls [artistList()]
   @override
   Widget build(BuildContext context) {
     return songContainer();}
-   /* return FutureBuilder(
-        future: API()
-            .authenticate()
-            .tracks
-            .get(trackID),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            Track track = snapshot.data;
-            print('track is $track from builder');
-            return GestureDetector(
-              onTap: onTap(),
-
-              child: Container(
-              alignment: Alignment.center,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.all(20),
-                        child:
-                        ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          child:
-                          FadeInImage.memoryNetwork(
-                              placeholder: kTransparentImage, image: API().imageUrl(track.album!)),
-                        )),
-                    Text('${track.name}',
-                      style: TextStyle(fontFamily: 'Acme', fontSize: 18,),
-                      textAlign: TextAlign.center,),
-                    artistList(track),
-                  ]
-
-              )
-            ),
-            );
-          }
-          else {
-            return Center(
-                   child: Text('Not available',
-                      style: TextStyle(fontFamily: 'Acme', fontSize: 20,),
-                      textAlign: TextAlign.center,),
-                 // ]
-
-             // ),
-            );*/
           }
 
 
@@ -317,19 +204,4 @@ void getCurrentTrack()  {
 }
 */
 
-/*
-void goToSongPage(SpotifyApi spotify, String songID) {
-  Track? track;
-  spotify.tracks.get(songID).then((value) {
-    track = value;
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => Song(
-          track: track!,
-        ),
-      ),
-    );
-  });
-}
-*/
+
